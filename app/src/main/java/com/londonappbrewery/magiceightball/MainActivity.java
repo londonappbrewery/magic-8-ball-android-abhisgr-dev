d557d9122385041041824875cca0e92316bc0862
AbhiSagar <abhisgr101.dev@gmail.com>
2020-07-07 13:04:55 +0530

    Completed The Magic 8 Ball Assignment

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,41 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class
+MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final int[] fortuneImageArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5,
+        };
+
+        final ImageView fortuneImage = findViewById(R.id.fortuneImage);
+
+        final Button askButton = findViewById(R.id.askButton);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int imageNum = randomNumberGenerator.nextInt(5);
+                Log.d("8ball", "Button Pressed. The Random Number is: " + imageNum);
+                fortuneImage.setImageResource(fortuneImageArray[imageNum]);
+            }
+        });
     }
 }
